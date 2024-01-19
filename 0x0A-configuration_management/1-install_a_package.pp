05dde854ca4af938515915a025f34d9459f7f3f6
mina-william-23 <capeandroid50@gmail.com>
2024-01-19 19:34:26 +0200

    puppet tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,7 @@
-# Install Python 3.8.10
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'deadsnakes-ppa',
-}
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# Install flask using pip3
+# ensure it is of version 2.1.0
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => [Package['python3.8'], Package['python3-pip'], Package['werkzeug']],
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
