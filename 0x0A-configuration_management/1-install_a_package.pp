09bb03677475d8f411fed51048d95078d644c6db
mina-william-23 <capeandroid50@gmail.com>
2024-01-19 18:10:37 +0200

    puppet tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,4 @@
-# Installs flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask 2.1.0 from pip3
+package{'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',}
