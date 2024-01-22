c3456d62a6217277589f017f07775e6c597fadaf
mina-william-23 <capeandroid50@gmail.com>
2024-01-22 03:54:36 +0200

    task 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Configure SSH client to use private key for authentication
 
 exec { 'identify_school_private_key':
-  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  command => `echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config`,
 }
 exec { 'disable_password_ssh_connection':
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command => `echo "PasswordAuthentication no" >> /etc/ssh/ssh_config`,
 }
