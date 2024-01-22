e354fde0e8579413d28ad3514f970bfbba6cf8e3
mina-william-23 <capeandroid50@gmail.com>
2024-01-22 03:46:49 +0200

    task 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
+
 exec { 'identify_school_private_key':
-  command => 'sudo echo "  IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  command => 'echo "  IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
 exec { 'disable_password_ssh_connection':
-  command => 'sudo echo "  PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command => 'echo "  PasswordAuthentication no" >> /etc/ssh/ssh_config',
 }
