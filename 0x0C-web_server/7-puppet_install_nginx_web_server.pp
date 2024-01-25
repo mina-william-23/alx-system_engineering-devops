7a9772e2a8c0f1b9de476ceb9cface7e4cda5e42
mina-william-23 <capeandroid50@gmail.com>
2024-01-25 03:42:12 +0200

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,32 @@
 # configure an Nginx with puppet
 
-package{ 'nginx':
+package { 'nginx':
   ensure => 'installed',
 }
 
-service{ 'nginx':
+service { 'nginx':
   ensure => 'running',
   enable => true,
 }
 
-exec {'redirect_me':
-	command  => 'sed -i "24i location \/redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}" /etc/nginx/sites-available/default',
-	provider => 'shell',
+exec { 'redirect_me':
+  command  => 'sed -i "24i location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}" /etc/nginx/sites-available/default',
+  provider => 'shell',
   require  => Package['nginx'],
   notify   => Service['nginx'],
 }
 
-exec {'404-error':
-	command  => 'sed -i "24i error_page 404 /custom_404.html;\n" /etc/nginx/sites-available/default" /etc/nginx/sites-available/default',
-	provider => 'shell',
+exec { '404-error':
+  command  => 'sed -i "24i error_page 404 /custom_404.html;" /etc/nginx/sites-available/default',
+  provider => 'shell',
   require  => Package['nginx'],
   notify   => Service['nginx'],
 }
 
-file {'/var/www/html/index.html':
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
-file {'/var/www/html/404.html':
+file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page",
 }
