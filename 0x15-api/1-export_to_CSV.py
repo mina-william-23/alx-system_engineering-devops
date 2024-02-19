ddaf5983da0f2de6a730bf4ee98c6b9862ebcbd4
mina-william-23 <capeandroid50@gmail.com>
2024-02-19 14:37:25 +0200

    task 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Gather data from an API"""
+import csv
 import requests
 from sys import argv
 
@@ -10,13 +11,13 @@ def export_csv(user_id):
     user = requests.get(url + "/users/{}".format(user_id)).json()
     todos = requests.get(url + "/todos", params={"userId": user_id}).json()
 
+    employee_username = user.get("username")
     with open("{}.csv".format(user_id), "w") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todos:
-            csv_file.write('"{}","{}","{}","{}"\n'.format(
-                user_id,
-                user.get("username"),
-                task.get("completed"),
-                task.get("title")))
+            row = (user_id, employee_username,
+                   task.get('completed'), task.get('title'))
+            writer.writerow(row)
 
 
 if __name__ == "__main__":
