32003454a89729a7e01aff0bb35b0e1b2278381b
mina-william-23 <capeandroid50@gmail.com>
2024-03-05 22:23:20 +0200

    trial 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""AdvancedAPI"""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Returns the top 10 hotest posts for a given subreddit"""
-    base_url = 'https://www.reddit.com'
-    query = 'r/{}/hot.json'.format(subreddit)
-    headers = {
-        "User-Agent": "random user"
-    }
-    params = {
-        "limit": 10
-    }
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
     req = requests.get(
-        url='{}/{}'.format(base_url, query),
-        headers=headers,
-        params=params,
-        allow_redirects=False
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
     )
-    if req.status_code >= 300:
-        print('None')
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        res = req.json().get('data').get('children')
-        for children in res:
-            print(children.get('data').get('title'))
+        print(None)
