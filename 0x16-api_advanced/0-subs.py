cf3c6065eb60fbd9f2a8d54a476e4e05610b3f9f
mina-william-23 <capeandroid50@gmail.com>
2024-03-05 21:30:59 +0200

    trial 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,6 @@ def number_of_subscribers(subreddit):
         headers=headers,
         # allow_redirects=False
     )
-    try:
-        res = req.json().get('data', None).get('subscribers', None)
-        return res if res else 0
-    except Exception as e:
-        return 0
+
+    res = req.json().get('data', None).get('subscribers', None)
+    return res if res else 0
