2442abd17991f65411e1a07766727a7b718f5a02
mina-william-23 <capeandroid50@gmail.com>
2024-03-05 21:14:36 +0200

    trial 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,20 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """AdvancedAPI"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of suscriber for a given subreddit"""
+    """Returns the number of suscribers"""
     base_url = 'https://www.reddit.com'
     query = 'r/{}/about.json'.format(subreddit)
     headers = {
-        "User-Agent": "linux:hbtn.advanced.api (by /u/koeusiss)"
+        "User-Agent": "random user"
     }
     req = requests.get(
         url='{}/{}'.format(base_url, query),
         headers=headers,
     )
-    # if req.status_code != 200:
-    #    return 0
-    try :
+    try:
         res = req.json()
         return res.get('data').get('subscribers')
     except Exception as e:
