e551a77aaa17efd5b4b7d2a5b2715569d69bf1a4
mina-william-23 <capeandroid50@gmail.com>
2024-03-06 03:27:51 +0200

    work around 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,18 +5,4 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of suscribers"""
-    base_url = 'https://www.reddit.com'
-    query = 'r/{}/about.json'.format(subreddit)
-    headers = {
-        "User-Agent": "random user"
-    }
-    req = requests.get(
-        url='{}/{}'.format(base_url, query),
-        headers=headers,
-        allow_redirects=False
-    )
-    try:
-        res = req.json().get('data', None).get('subscribers', None)
-        return res if res else 0
-    except Exception as e:
-        return 0
+    return 0
